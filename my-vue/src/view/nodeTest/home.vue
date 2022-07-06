f990415f6672763978d93e5fc98dbc323deff255
杜国强 <duguoqiang@weioutech.com>
2022-07-06 19:06:17 +0800

    viewerjs 实现图片预览

--- my-vue/src/view/nodeTest/home.vue
+++ my-vue/src/view/nodeTest/home.vue
@@ -2,12 +2,15 @@
     <div class="block">
         <el-carousel height="400px">
             <el-carousel-item v-for="item in homePicture" :key="item">
-                <img :src="item"></img>
+                <img :src="item" @click="viewImg(item)"></img>
             </el-carousel-item>
         </el-carousel>
+        <img ref="pictureDom" v-show="flase"></img>
   </div>
 </template>
 <script>
+import Viewer from 'viewerjs'
+import 'viewerjs/dist/viewer.css'
 export default {
     name: 'home',
     data(){
@@ -19,6 +22,17 @@ export default {
         this.$api.nodeAPI.homePicture().then((res)=>{
             this.homePicture = res.data.url
         })
+    },
+    methods:{
+        viewImg(url){
+            const dom = this.$refs.pictureDom
+            dom.src = url
+            console.log(dom,111)
+            var image = new Viewer(dom,{
+
+            });
+            dom.click()
+        }
     }
 }
 </script>
